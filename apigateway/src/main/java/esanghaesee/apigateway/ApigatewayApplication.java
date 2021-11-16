f93ecc5c148086831a0c7e9be625b030d5ac5c3b
jongbin <runnz121@gmail.com>
2021-11-17 03:39:28 +0900

    first commit

---
+++ apigateway/src/main/java/esanghaesee/apigateway/ApigatewayApplication.java
@@ -0,0 +1,13 @@
+package esanghaesee.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ApigatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApigatewayApplication.class, args);
+	}
+}
